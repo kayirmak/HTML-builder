0f63e1b3fa1961b85f134591f43f163c09699af0
Kairat <dzhumabaev.kai@gmail.com>
2022-05-24 17:24:41 +0600

    feat: add second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,22 +8,29 @@ fs.access(path.resolve(__dirname, 'text.txt'), err => {
         fs.writeFile(path.resolve(__dirname, 'text.txt'), '', err => {
             if (err) console.log(err.message);
             console.log('Файл создан');
+            stdout.write('Напиши любимую музыку\n')
         })
     }
     else {
-        console.log('Файл найден');
+        stdout.write('Напиши любимую музыку\n')
     }
 })
 
-const txt = fs.createWriteStream(path.resolve(__dirname, 'text.txt'))
 
-stdout.write('Напишите любимый цвет')
 stdin.on('data', data => {
-    console.log(data.toString());
     if (data.toString().includes('exit')) {
-        stdout.write('yesexit');
+        stdout.write('Спасибо, у тебя хороший вкус, хотя о вкусах не спорят');
         process.exit()
     }
+    fs.appendFile(path.resolve(__dirname, 'text.txt'), data.toString(), error => {
+        if (error) throw error
+        stdout.write('Запись добавлена, для завершения процесса введи "exit"\n')
+    })
+    // process.openStdin().on('keypress', function (chunk, key) {
+    //     if (key && key.ctrl && key.name == 'c') {
+    //         console.log('bye');
+    //         process.exit()
+    //     }
+    // })
 })
-// process.on('exit', () => stdin.write('Пока'))
-// const txt = fs.createWriteStream()
+process.on('exit', () => stdin.write('Пока'))
