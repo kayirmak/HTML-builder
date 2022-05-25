3c2e9e3cd93a4da8269ce5142d6a5b84a9b9717c
Kairat <dzhumabaev.kai@gmail.com>
2022-05-25 16:22:24 +0600

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+const {copyFile, mkdir, readdir} = require('fs/promises');
+const { stdout } = require('process');
+
+async function copy() {
+    const res = await mkdir(path.resolve(__dirname, 'files-copy'), {recursive: true})
+    if (res) {
+        stdout.write('Папка успешно создана\n')
+    }
+    
+    const files = await readdir(path.resolve(__dirname, 'files'), {withFileTypes: true})
+
+    files.forEach(el => {
+        copyFile(path.resolve(__dirname, 'files', el.name), `${__dirname}/files-copy/${el.name}`, fs.constants.COPYFILE_EXCL).then(res => {
+            stdout.write('Файл скопирован\n')
+        }).catch(err => {
+            stdout.write('Файл уже существует\n');
+        })
+    })    
+}
+copy()
 
