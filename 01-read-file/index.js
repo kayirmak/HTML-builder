d985bceb0d0fc2f24a7702a559d791b71b8605f1
Kairat <dzhumabaev.kai@gmail.com>
2022-05-26 17:19:19 +0600

    fix: 1-task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,5 +6,4 @@ let txt = fs.createReadStream(path.resolve(__dirname, 'text.txt'), 'utf-8');
 let data = '';
 txt.on('data', chunk => data += chunk);
 txt.on('end', () => console.log(data));
-console.log('Пожалуйста дай шанс до окончания cross-check, доделать');
 txt.on('error', error => console.log('Error', error.message));
